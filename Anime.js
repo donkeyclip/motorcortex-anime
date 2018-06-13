78d7474329d7549e0cf02c7b458c13e68765a97b
Ioannis Potouridis <potouridisio@gmail.com>
2018-06-13 10:57:11 +0300

    Fixed undefined attrs

--- Anime.js
+++ Anime.js
@@ -11,17 +11,19 @@ class Anime extends MotorCortex.TimedIncident {
         const compoAttribute = this.channel.compoAttributes[key];
 
         for (var i = 0; i <= compoAttribute.length; i++) {
-          if (!attrs[key].hasOwnProperty(compoAttribute[i])) {
+          if (
+            !this.attrs.animatedAttrs[key].hasOwnProperty(compoAttribute[i])
+          ) {
             continue;
           }
           x[compoAttribute[i]] = [
             this.getInitialValue(key)[compoAttribute[i]],
-            attrs[key][compoAttribute[i]]
+            this.attrs.animatedAttrs[key][compoAttribute[i]]
           ];
           z[key] = anime.getValue(this.element, compoAttribute[i]);
         }
       } else {
-        x[key] = [this.getInitialValue(key), attrs[key]];
+        x[key] = [this.getInitialValue(key), this.attrs.animatedAttrs[key]];
         z[key] = anime.getValue(this.element, key);
       }
     }
